009be638fd100a998297008e5e108ff46c03fbd5
krishna10-dev <kk1930778@gmail.com>
2025-12-12 17:39:47 +0530

    Allow all CORS origins

--- server/index.js
+++ server/index.js
@@ -18,10 +18,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:3000', 
-    'https://full-stack-productr.netlify.app/login'
-  ],
+  origin: '*',
   credentials: true
 }));
 app.use(express.json());
